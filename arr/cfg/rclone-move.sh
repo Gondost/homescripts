4586c7cb5e8005ebdf9ed06aadc25bd072c97647
Gondost <1337357@gmail.com>
2019-11-05 20:10:05 -0500

    Update rclone-move.sh

--- arr/cfg/rclone-move.sh
+++ arr/cfg/rclone-move.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
-SABMOVIEDIR="/arr/dl/done/movies"
-SONARRDIR="/arr/dl/done/sonarr_out"
-MOVIESTOSYNCSZ=`find $SABMOVIEDIR -mindepth 1 -type d -not -empty -not -name "_UNPACK*" -print`
-MOVIESTOSYNCRR=`find $SABMOVIEDIR -mindepth 1 -type f -not -name "*.partial~" -print`
-TVTOSYNCSZ=`find $SONARRDIR -mindepth 1 -type d -not -empty -not -name "_UNPACK*" -print`
-TVTOSYNCRR=`find $SONARRDIR -mindepth 1 -type f -not -name "*.partial~" -print`
+MOVIEDIR="/arr/dl/done/radarr_out"
+TVDIR="/arr/dl/done/sonarr_out"
+MOVIESTOSYNCSZ=`find $MOVIEDIR -mindepth 1 -type d -not -empty -not -name "_UNPACK*" -print`
+MOVIESTOSYNCRR=`find $MOVIEDIR -mindepth 1 -type f -not -name "*.partial~" -print`
+TVTOSYNCSZ=`find $TVDIR -mindepth 1 -type d -not -empty -not -name "_UNPACK*" -print`
+TVTOSYNCSR=`find $TVDIR -mindepth 1 -type f -not -name "*.partial~" -print`
 
 # Sabnzbd movie done directory
 if [[ "$MOVIESTOSYNCSZ" != "" || "$MOVIESTOSYNCRR" != "" ]]; then
-        echo "INFO: $SABMOVIEDIR has items to sync, checking rclone move status"
+        echo "INFO: $MOVIEDIR has items to sync, checking rclone move status"
         RCLONEMOVESTATUS=`ps -ef | grep "rclone move" | grep -v 'grep'`
         if [[ "$RCLONEMOVESTATUS" != "" ]]; then
                 echo "INFO: rclone move already running, skipping sync"
         else
-                echo "INFO: rclone move inactive, running on $SABMOVIEDIR"
-                rclone move $SABMOVIEDIR gc_one:/media/movies\
-                 -P -v --transfers 1 --exclude "/*unpack*/**" --exclude "*.partial~"\
+                echo "INFO: rclone move inactive, running on $MOVIEDIR"
+                rclone move $MOVIEDIR gc_one:/media/movies\
+                 -P --transfers 2 --exclude "/*unpack*/**" --exclude "*.partial~"\
                  --ignore-case --fast-list --delete-empty-src-dirs\
-                 && find $SABMOVIEDIR -mindepth 1 -type d -empty -print -delete
+                 && find $MOVIEDIR -mindepth 1 -type d -empty -print -delete
         fi
 else
-        echo "INFO: $SABMOVIEDIR has no eligible items to move"
+        echo "INFO: $MOVIEDIR has no eligible items to move"
 fi
 
 # Sonarr sorted TV shows directory
-if [[ "$TVTOSYNCSZ" != "" || "$TVTOSYNCRR" != "" ]]; then
-        echo "INFO: $SONARRDIR has items to sync, checking rclone move status"
+if [[ "$TVTOSYNCSZ" != "" || "$TVTOSYNCSR" != "" ]]; then
+        echo "INFO: $TVDIR has items to sync, checking rclone move status"
         RCLONEMOVESTATUS=`ps -ef | grep "rclone move" | grep -v 'grep'`
-        if [[ "$RCLONEMOVERSTATUS" != "" ]]; then
+        if [[ "$RCLONEMOVESTATUS" != "" ]]; then
                 echo "INFO: rclone move already running, skipping sync"
         else
-                echo "INFO: rclone move inactive, running on $SONARRDIR"
-                rclone move $SONARRDIR gc_one:/media/tv\
-                 -P -v --transfers 2 --exclude "/*unpack*/**" --exclude "*.partial~"\
+                echo "INFO: rclone move inactive, running on $TVDIR"
+                rclone move $TVDIR gc_one:/media/tv\
+                 -P --transfers 4 --exclude "/*unpack*/**" --exclude "*.partial~"\
                  --ignore-case --fast-list --delete-empty-src-dirs\
-                 && find $SONARRDIR -mindepth 1 -type d -empty -print -delete
+                 && find $TVDIR -mindepth 1 -type d -empty -print -delete
         fi
 else
-        echo "INFO: $SONARRDIR has no eligible items to move"
+        echo "INFO: $TVDIR has no eligible items to move"
 fi
